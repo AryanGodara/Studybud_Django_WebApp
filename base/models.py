0c05db8840bced80430c986fe36f205c9a44d650
Aryan <aryangodara03@gmail.com>
2022-01-30 18:30:51 +0530

    Topic, User, Message Models finalized

--- base/models.py
+++ base/models.py
@@ -1,12 +1,22 @@
-from ssl import create_default_context
-from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+    
+    #? A topic can have multiple rooms, but a room can only have one topic
+    #* There can be many places(rooms) to discuss a topic(say, anime). But an 'anime room'
+    #* should only be used to discuss anime, not anything else
 class Room(models.Model):
-    # host = 
-    # topic = 
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+        # The person that hosts the room
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+        # The topic of conversation inside the room
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     # participants =    Stores all the users that are currently active in the room
@@ -14,16 +24,17 @@ class Room(models.Model):
     created = models.DateTimeField(auto_now_add=True)   # Only updated when created
     
     def __str__(self):
-        return str(self.name)
+        return self.name
     
 
 class Message(models.Model):
-    # user = 
-    room = models.ForeignKey(Room, on_delete=CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+        #? If a user is deleted, delete all of its messages 
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
         #? If a room is deleted, Delete all the message inside the room
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)       # Updated on every updation
     created = models.DateTimeField(auto_now_add=True)   # Only updated when created
     
     def __str__(self):
-        return str(self.body[0:50])     #* Only the first 50 characters in the preview of message
+        return self.body[0:50]     #* Only the first 50 characters in the preview of message
