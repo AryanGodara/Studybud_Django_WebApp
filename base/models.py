f9edbaf740bd77f3c072dc279a9572b95bd80eef
Aryan <aryangodara03@gmail.com>
2022-01-30 17:40:25 +0530

    Room model created

--- base/models.py
+++ base/models.py
@@ -1,3 +1,16 @@
+from ssl import create_default_context
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    # host = 
+    # topic = 
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # participants =    Stores all the users that are currently active in the room
+    updated = models.DateTimeField(auto_now=True)       # Updated on every updation
+    created = models.DateTimeField(auto_now_add=True)   # Only updated when created
+    
+    def __str__(self):
+        return str(self.name)
