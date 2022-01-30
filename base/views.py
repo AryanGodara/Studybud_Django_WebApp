65107c4c16f38fe43707fa6ef20ef22fa2295aa8
Aryan <aryangodara03@gmail.com>
2022-01-30 17:07:17 +0530

    rooms added using context dictionary

--- base/views.py
+++ base/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Lets Learn Python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Fronted Developer'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms':rooms})
+
 def room(request):
     return render(request, 'room.html')
