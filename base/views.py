d9cce6057bee2c2182255218552604c9c5ab6c3c
Aryan <aryangodara03@gmail.com>
2022-01-30 17:14:37 +0530

    <str:pk> added to room view

--- base/views.py
+++ base/views.py
@@ -12,5 +12,5 @@ def home(request):
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)
 
-def room(request):
+def room(request,pk):
     return render(request, 'base/room.html')
