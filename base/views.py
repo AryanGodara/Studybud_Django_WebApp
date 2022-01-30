ffa83e599d11459d97506e6c93900125c3ec83a0
Aryan <aryangodara03@gmail.com>
2022-01-30 16:31:38 +0530

    render instead of httpresponse

--- base/views.py
+++ base/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'home.html')
 def room(request):
-    return HttpResponse('Room')
+    return render(request, 'room.html')
