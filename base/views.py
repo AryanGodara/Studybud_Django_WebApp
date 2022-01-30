04d45629d510a9488e05424dde85ae2533dd2ad9
Aryan <aryangodara03@gmail.com>
2022-01-30 18:40:24 +0530

    createRoom view created

--- base/views.py
+++ base/views.py
@@ -16,3 +16,8 @@ def room(request,pk):
 
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    
+    context = {}
+    return render(request, 'base/room.html', context)
