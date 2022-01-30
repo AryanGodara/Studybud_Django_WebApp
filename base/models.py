1d8b212ce219370f5ac3e7d86697bbd4e48341ae
Aryan <aryangodara03@gmail.com>
2022-01-30 18:19:06 +0530

    Message model added

--- base/models.py
+++ base/models.py
@@ -1,4 +1,5 @@
 from ssl import create_default_context
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
@@ -14,3 +15,15 @@ class Room(models.Model):
     
     def __str__(self):
         return str(self.name)
+    
+
+class Message(models.Model):
+    # user = 
+    room = models.ForeignKey(Room, on_delete=CASCADE)
+        #? If a room is deleted, Delete all the message inside the room
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)       # Updated on every updation
+    created = models.DateTimeField(auto_now_add=True)   # Only updated when created
+    
+    def __str__(self):
+        return str(self.body[0:50])     #* Only the first 50 characters in the preview of message
