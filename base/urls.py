c4450a1f9599bb9231c9c4ce301ccfa7fab42952
Aryan <aryangodara03@gmail.com>
2022-01-30 16:22:37 +0530

    added paths to room and home

--- base/urls.py
+++ base/urls.py
@@ -1,1 +1,8 @@
+from unicodedata import name
 from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('room/', views.room, name="room"),
+]
