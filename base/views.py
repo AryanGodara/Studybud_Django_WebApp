45a20e2f7190a831cd6e4b5c80d27a19c6d81bbc
Aryan <aryangodara03@gmail.com>
2022-01-30 21:18:23 +0530

    login working f yeah

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 from .models import Room, Topic
@@ -20,6 +21,14 @@ def loginPage(request):
             user = User.objects.get(username=username)
         except:
             messages.error(request, 'User does not exist')
+            
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR Password does not exist')
     
     context = {}
     return render(request, 'base/login_register.html', context)
