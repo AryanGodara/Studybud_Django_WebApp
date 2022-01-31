f99bc5a6089e446e98c3b076786b2029521fbf2b
Aryan <aryangodara03@gmail.com>
2022-01-31 20:26:47 +0530

    website complete, only user customization left

--- studybud/settings.py
+++ studybud/settings.py
@@ -140,3 +140,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True   #? Now any random website/URL can access our APIs
